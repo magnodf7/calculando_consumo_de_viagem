1f6206eae8e9321f90e8bf602ad51068a3977b7a
magnodf7 <magno.brunodf7@gmail.com>
2024-01-05 20:53:56 -0300

    primeiro commit

---
+++ aula2.js
@@ -0,0 +1,7 @@
+const numero = 23;
+
+const numeroPar = (numero % 2) === 0;
+
+console.log(numeroPar);
+
+//verifica os numero e são par ou ipar
---
+++ aula3.js
@@ -0,0 +1,12 @@
+const numero = 25;
+
+const numeroPar = (numero % 2) === 0;
+console.log(numeroPar);
+
+if (numeroPar)  {
+    console.log("El número es par");
+}else{
+    console.log("El número no es par");
+}
+
+//fazendo estrutura if e else
